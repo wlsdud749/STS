618b0b94ec77e0e2ea7a1c0c950b535290b94b4e
DESKTOP-VPE9I76\SW중심대학사업단_24 <sth7492@naver.com>
2023-03-06 15:58:45 +0900

    프로젝트 생성 , lombok 추가

---
+++ springboot_ex01/src/main/java/org/comstudy/saram/SpringbootEx01Application.java
@@ -0,0 +1,13 @@
+package org.comstudy.saram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEx01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEx01Application.class, args);
+	}
+
+}
---
+++ springboot_ex01/src/test/java/org/comstudy/saram/SpringbootEx01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.comstudy.saram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEx01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
